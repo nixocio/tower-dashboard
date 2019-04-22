2c34eb2973a9868a10cd450df286a9551dca0545
Yanis Guenane <yanis@guenane.org>
2019-04-22 22:02:58 +0200

    Release 3.5: Create new entries and remove 3.2 ones

--- towerdashboard/data/base.py
+++ towerdashboard/data/base.py
@@ -18,6 +18,7 @@
 
 ANSIBLE_VERSIONS = [
     {'name': 'devel'},
+    {'name': 'stable-2.8'},
     {'name': 'stable-2.7'},
     {'name': 'stable-2.6'},
     {'name': 'stable-2.5'},
@@ -31,7 +32,7 @@ OS_VERSIONS = [
     {'name': 'rhel-7.5-x86_64', 'desc': 'RHEL 7.5', 'family': 'RHEL'},
     {'name': 'rhel-7.4-x86_64', 'desc': 'RHEL 7.4', 'family': 'RHEL'},
     {'name': 'centos-7.latest-x86_64', 'desc': 'CentOS Latest', 'family': 'RHEL'},  # noqa
-    {'name': 'ol-7.5-x86_64', 'desc': 'Oracle Linux 7.5', 'family': 'RHEL'},
+    {'name': 'ol-7.6-x86_64', 'desc': 'Oracle Linux 7.5', 'family': 'RHEL'},
     {'name': 'ubuntu-16.04-x86_64', 'desc': 'Ubuntu 16.04', 'family': 'Ubuntu'},  # noqa
     {'name': 'ubuntu-14.04-x86_64', 'desc': 'Ubuntu 14.04', 'family': 'Ubuntu'},  # noqa
 ]
@@ -42,6 +43,15 @@ TOWER_VERSIONS = [
     {
         'name': 'In Development',
         'code': 'devel',
+        'general_availability': None,
+        'end_of_full_support': None,
+        'end_of_maintenance_support': None,
+        'end_of_life': None,
+        'spreadsheet_url': 'https://docs.google.com/spreadsheets/d/1NNTN-SBM23UQPZAH9HylKhYQBAoyIsDcTlvl_ItDzHs/edit#gid=762158314'
+    },
+    {
+        'name': 'Release 3.5',
+        'code': '3.5',
         'general_availability': None,
         'end_of_full_support': None,
         'end_of_maintenance_support': None,
@@ -66,15 +76,6 @@ TOWER_VERSIONS = [
         'end_of_life': '2020-03-12',
         'spreadsheet_url': None
     },
-    {
-        'name': 'Release 3.2',
-        'code': '3.2',
-        'general_availability': '2017-10-02',
-        'end_of_full_support': '2018-04-02',
-        'end_of_maintenance_support': '2018-10-02',
-        'end_of_life': '2019-04-02',
-        'spreadsheet_url': None
-    },
 ]
 
 TOWER_OS = [
@@ -83,42 +84,52 @@ TOWER_OS = [
     {'tower': 'In Development', 'os': 'rhel-7.5-x86_64'},
     {'tower': 'In Development', 'os': 'rhel-7.4-x86_64'},
     {'tower': 'In Development', 'os': 'centos-7.latest-x86_64'},
-    {'tower': 'In Development', 'os': 'ol-7.5-x86_64'},
+    {'tower': 'In Development', 'os': 'ol-7.6-x86_64'},
     {'tower': 'In Development', 'os': 'ubuntu-16.04-x86_64'},
+
+    {'tower': 'Release 3.5', 'os': 'rhel-8.0-x86_64'},
+    {'tower': 'Release 3.5', 'os': 'rhel-7.6-x86_64'},
+    {'tower': 'Release 3.5', 'os': 'rhel-7.5-x86_64'},
+    {'tower': 'Release 3.5', 'os': 'rhel-7.4-x86_64'},
+    {'tower': 'Release 3.5', 'os': 'centos-7.latest-x86_64'},
+    {'tower': 'Release 3.5', 'os': 'ol-7.6-x86_64'},
+    {'tower': 'Release 3.5', 'os': 'ubuntu-16.04-x86_64'},
 
     {'tower': 'Release 3.4', 'os': 'rhel-7.6-x86_64'},
     {'tower': 'Release 3.4', 'os': 'rhel-7.5-x86_64'},
     {'tower': 'Release 3.4', 'os': 'rhel-7.4-x86_64'},
     {'tower': 'Release 3.4', 'os': 'centos-7.latest-x86_64'},
-    {'tower': 'Release 3.4', 'os': 'ol-7.5-x86_64'},
+    {'tower': 'Release 3.4', 'os': 'ol-7.6-x86_64'},
     {'tower': 'Release 3.4', 'os': 'ubuntu-16.04-x86_64'},
 
     {'tower': 'Release 3.3', 'os': 'rhel-7.6-x86_64'},
     {'tower': 'Release 3.3', 'os': 'rhel-7.5-x86_64'},
     {'tower': 'Release 3.3', 'os': 'rhel-7.4-x86_64'},
     {'tower': 'Release 3.3', 'os': 'centos-7.latest-x86_64'},
-    {'tower': 'Release 3.3', 'os': 'ol-7.5-x86_64'},
+    {'tower': 'Release 3.3', 'os': 'ol-7.6-x86_64'},
     {'tower': 'Release 3.3', 'os': 'ubuntu-16.04-x86_64'},
     {'tower': 'Release 3.3', 'os': 'ubuntu-14.04-x86_64'},
-
-    {'tower': 'Release 3.2', 'os': 'rhel-7.6-x86_64'},
-    {'tower': 'Release 3.2', 'os': 'rhel-7.5-x86_64'},
-    {'tower': 'Release 3.2', 'os': 'rhel-7.4-x86_64'},
-    {'tower': 'Release 3.2', 'os': 'centos-7.latest-x86_64'},
-    {'tower': 'Release 3.2', 'os': 'ol-7.5-x86_64'},
-    {'tower': 'Release 3.2', 'os': 'ubuntu-16.04-x86_64'},
-    {'tower': 'Release 3.2', 'os': 'ubuntu-14.04-x86_64'},
 ]
 
 TOWER_ANSIBLE = [
     {'tower': 'In Development', 'ansible': 'devel'},
+    {'tower': 'In Development', 'ansible': 'stable-2.8'},
     {'tower': 'In Development', 'ansible': 'stable-2.7'},
     {'tower': 'In Development', 'ansible': 'stable-2.6'},
     {'tower': 'In Development', 'ansible': 'stable-2.5'},
     {'tower': 'In Development', 'ansible': 'stable-2.4'},
     {'tower': 'In Development', 'ansible': 'stable-2.3'},
 
+    {'tower': 'Release 3.5', 'ansible': 'devel'},
+    {'tower': 'Release 3.5', 'ansible': 'stable-2.8'},
+    {'tower': 'Release 3.5', 'ansible': 'stable-2.7'},
+    {'tower': 'Release 3.5', 'ansible': 'stable-2.6'},
+    {'tower': 'Release 3.5', 'ansible': 'stable-2.5'},
+    {'tower': 'Release 3.5', 'ansible': 'stable-2.4'},
+    {'tower': 'Release 3.5', 'ansible': 'stable-2.3'},
+
     {'tower': 'Release 3.4', 'ansible': 'devel'},
+    {'tower': 'Release 3.4', 'ansible': 'stable-2.8'},
     {'tower': 'Release 3.4', 'ansible': 'stable-2.7'},
     {'tower': 'Release 3.4', 'ansible': 'stable-2.6'},
     {'tower': 'Release 3.4', 'ansible': 'stable-2.5'},
@@ -126,18 +137,12 @@ TOWER_ANSIBLE = [
     {'tower': 'Release 3.4', 'ansible': 'stable-2.3'},
 
     {'tower': 'Release 3.3', 'ansible': 'devel'},
+    {'tower': 'Release 3.3', 'ansible': 'stable-2.8'},
     {'tower': 'Release 3.3', 'ansible': 'stable-2.7'},
     {'tower': 'Release 3.3', 'ansible': 'stable-2.6'},
     {'tower': 'Release 3.3', 'ansible': 'stable-2.5'},
     {'tower': 'Release 3.3', 'ansible': 'stable-2.4'},
     {'tower': 'Release 3.3', 'ansible': 'stable-2.3'},
-
-    {'tower': 'Release 3.2', 'ansible': 'devel'},
-    {'tower': 'Release 3.2', 'ansible': 'stable-2.7'},
-    {'tower': 'Release 3.2', 'ansible': 'stable-2.6'},
-    {'tower': 'Release 3.2', 'ansible': 'stable-2.5'},
-    {'tower': 'Release 3.2', 'ansible': 'stable-2.4'},
-    {'tower': 'Release 3.2', 'ansible': 'stable-2.3'},
 ]
 
 RESULTS = [
